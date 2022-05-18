5dde9bd5d1473b921021703a70db0619571dd4e2
Ben <ringerben1@gmail.com>
2022-05-18 17:31:15 -0400

    Nodemon

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-const name = 'Ben';
+const express = require('express');
 
-console.log(name);
-const greet = (name) =>{
-    console.log(`Hello, ${name}`)
-}
+//express app
+const app = express();
 
-greet('Jim');
-greet('Bone')
+//listen for requests
+app.listen(3000);
+
+app.get('/', (req,res) => {
+    // res.send('<h1>Home</h1>');
+    res.sendFile('./views/index.html', {root: __dirname});
+});
+
+app.get('/about', (req,res) => {
+    // res.send('<h1>About</h1>');
+    res.sendFile('./views/about.html', {root: __dirname});
+});
+
+
+//redirects
+app.get('/about-us', (req,res) => {
+    res.redirect('/about');
+});
+
+//404 page
+app.use((req,res) =>{
+    res.status(404).sendFile('./views/404.html', {root: __dirname})
+});
