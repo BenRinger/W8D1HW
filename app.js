de8b9adf49aa68486897078799fbb4e9d11ce212
Ben <ringerben1@gmail.com>
2022-05-19 17:50:01 -0400

    7 & 8

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 const express = require('express');
+const morgan = require('morgan');
 
 //express app
 const app = express();
 
+//register view engine
+app.set('view engine','ejs');
+
 //listen for requests
 app.listen(3000);
 
+//middleware & static files
+
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+
+
 app.get('/', (req,res) => {
-    // res.send('<h1>Home</h1>');
-    res.sendFile('./views/index.html', {root: __dirname});
+    const blogs = [
+        {title:'Feeding your fish', snippet:'Lorem ipsum dolor sit amet consectetur'},
+        {title:'Walking on water', snippet:'Lorem ipsum dolor sit amet consectetur'},
+        {title:'Eating your foot', snippet:'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', {title: 'Home', blogs});
 });
 
 app.get('/about', (req,res) => {
     // res.send('<h1>About</h1>');
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About'});
 });
 
-
-//redirects
-app.get('/about-us', (req,res) => {
-    res.redirect('/about');
-});
+app.get('/blogs/create',(req,res) => {
+    res.render('create', {title: 'Create a new blog'});
+})
 
 //404 page
 app.use((req,res) =>{
-    res.status(404).sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404', {title: '404'})
 });
