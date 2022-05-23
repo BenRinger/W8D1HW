e5f8b7fce91a557333e41906f1ba700ebc2315d0
Ben <ringerben1@gmail.com>
2022-05-23 08:51:14 -0400

    final

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { render } = require('ejs');
+const blogRoutes = require('./routes/blogRoutes')
 
+ 
 //express app
 const app = express();
+
+//connect to mongo db
+const dbURI =  'mongodb+srv://ringerben1:902910934Br@cluster0.avo0t.mongodb.net/node-tuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 //register view engine
 app.set('view engine','ejs');
 
-//listen for requests
-app.listen(3000);
-
 //middleware & static files
-
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
 app.use(morgan('dev'));
 
-
-
+//routes
 app.get('/', (req,res) => {
-    const blogs = [
-        {title:'Feeding your fish', snippet:'Lorem ipsum dolor sit amet consectetur'},
-        {title:'Walking on water', snippet:'Lorem ipsum dolor sit amet consectetur'},
-        {title:'Eating your foot', snippet:'Lorem ipsum dolor sit amet consectetur'},
-    ];
-    res.render('index', {title: 'Home', blogs});
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req,res) => {
-    // res.send('<h1>About</h1>');
     res.render('about', {title: 'About'});
 });
 
-app.get('/blogs/create',(req,res) => {
-    res.render('create', {title: 'Create a new blog'});
-})
+//blog routes
+app.use('/blogs', blogRoutes);
 
 //404 page
 app.use((req,res) =>{
